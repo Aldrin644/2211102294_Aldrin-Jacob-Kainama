9a351daa0a46c612e78613b4ffa1f8e8d8fb60cd
Aldrin644 <70737847+Aldrin644@users.noreply.github.com>
2025-04-28 11:56:56 +0700

    ABP PHP

--- lib/PHP/Tugas/proses_login.php
+++ lib/PHP/Tugas/proses_login.php
@@ -8,7 +8,7 @@ $password = $_POST['password'];
 
 if ($username === $valid_username && $password === $valid_password) {
     $_SESSION['username'] = $username;
-    header("Location: form.php");
+    header("Location: dashboard.php");
 } else {
     echo "<script>alert('Login Gagal!'); window.location.href='login.php';</script>";
 }
